f0724175a59e35f75fe137f075215781d0929d4f
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-05-14 16:13:42 +0200

    2.3

--- src/Lesson2/app.js
+++ src/Lesson2/app.js
@@ -3,16 +3,26 @@
 		{
 			name: 'Azurite',
 			price: 2.95,
-			canPurchase: false,
-			soldOut: true
+			images: [
+				image1.png,
+				image2.png
+			]
 		},
 		{
 			name: 'Bloodstone',
-			price: 5.95
+			price: 5.95,
+			images: [
+				image1.png,
+				image2.png
+			]
 		},
 		{
 			name: 'Zircon',
-			price: 3.95
+			price: 3.95,
+			images: [
+				image1.png,
+				image2.png
+			]
 		}
 	];
 
