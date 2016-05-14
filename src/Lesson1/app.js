bf78741d5b74470cf2ce202b2a8c7a45391f4dae
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-05-14 13:01:55 +0200

    1.7

--- src/Lesson1/app.js
+++ src/Lesson1/app.js
@@ -1,14 +1,24 @@
 (function() {
-	var gem = {
-		name: 'Azurite',
-		price: 2.95,
-		canPurchase: false,
-		soldOut: true
-	};
+	var gems = [
+		{
+			name: 'Azurite',
+			price: 2.95,
+			canPurchase: false,
+			soldOut: true
+		},
+		{
+			name: 'Bloodstone',
+			price: 5.95
+		},
+		{
+			name: 'Zircon',
+			price: 3.95
+		}
+	];
 
 	var app = angular.module('gemStore', []);
 
 	app.controller('StoreController', function() {
-		this.product = gem;
+		this.products = gems;
 	});
 })();
