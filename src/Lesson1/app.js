965848ef60c3d6be2dd15c7e141d7c5f5ebd3c19
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-05-14 12:41:34 +0200

    1.4

--- src/Lesson1/app.js
+++ src/Lesson1/app.js
@@ -1,1 +1,12 @@
-var app = angular.module('gemStore', []);
+(function() {
+	var gem = {
+		name: 'Azurite',
+		price: 2.95
+	};
+
+	var app = angular.module('gemStore', []);
+
+	app.controller('StoreController', function() {
+		this.product = gem;
+	});
+})();
